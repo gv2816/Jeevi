b35bdceaaa97e3694ed28f5d7fc40af50a7155c7
gv2816 <jeevithamohan28@gmail.com>
2020-05-25 19:49:19 +0530

    my project

---
+++ src/test/java/gv/gv.java
@@ -0,0 +1,17 @@
+package gv;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class gv {
+	
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\jm51\\eclipse-workspace\\prjt1\\driver\\chromedriver.exe");
+	
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.google.com/");
+	driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("Jeevitha");
+}
+
+}
